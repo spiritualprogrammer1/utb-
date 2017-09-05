21cb81f9db1da88613e0e2292a0caa4542c4d5c9
Lutte Patrick OKAMANGO <lu2tepok@outlook.com>
2017-09-05 09:38:48 +0000

    Good until now

---
+++ app/Diagnostic.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnostic extends Model
+{
+    protected $fillable = ['ids', 'reference', 'state', 'type', 'process_id', 'user_id'];
+
+    public function process()
+    {
+        return $this->belongsTo(Process::class);
+    }
+
+    public function diagnostic_employee()
+    {
+        return $this->hasMany(Diagnostic_employee::class);
+    }
+
+    public function demand()
+    {
+        return $this->hasMany(Demand::class);
+    }
+
+    public function repair()
+    {
+        return $this->hasMany(Repair::class);
+    }
+
+    public function visit_technique()
+    {
+        return $this->hasMany('App\Visit_technique');
+    }
+
+    function after_test()
+    {
+        return $this->hasMany('App\After_test');
+    }
+
+    public function revision()
+    {
+        return $this->hasMany('App\Revision');
+    }
+}
