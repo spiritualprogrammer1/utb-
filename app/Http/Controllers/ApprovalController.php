2ff53218ca57a4704d4b83607ab2a1721848045f
Lutte Patrick OKAMANGO <lu2tepok@outlook.com>
2017-09-14 16:14:19 +0000

    Good until now

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Approval;
 use App\Demand;
 use App\Diagnostic;
 use App\Demand_piece;
+use App\Repair;
+use App\Repair_description;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Validator;
 
 class ApprovalController extends Controller
@@ -17,7 +22,8 @@ class ApprovalController extends Controller
 
     public function index()
     {
-
+        $repairs = Repair::where('state', '5')->orderBy('updated_at', 'desc')->get();
+        return view('approval.home', ['repairs' => $repairs]);
     }
 
     public function create()
@@ -28,83 +34,128 @@ class ApprovalController extends Controller
 
     public function store(Request $request)
     {
-        //
-    }
+        if ($request->ajax()) {
+            $messages = array(
 
-    public function show(Request $request, $id)
-    {
-        if ($id == "waiting") {
-            $demands = Demand::where('state', '0')->orderBy('created_at', 'desc')->get();
-            return view('approval.includes.piece', ['demands' => $demands])->with(['id' => $id]);
-        } elseif ($id == "validated") {
-            $demands = Demand::where('state', '1')->orWhere('state', '2')->orWhere('state', '3')->orderBy('created_at', 'desc')->get();
-            return view('approval.includes.piece', ['demands' => $demands])->with(['id' => $id]);
+            );
+            $rules = array(
+                'diagnostic' => 'bail|required|min:1|max:255',
+                'id' => 'bail|required|min:1|max:255|',
+            );
+            $validator = Validator::make($request->all(), $rules, $messages);
+            if ($validator->fails()) {
+                $errors = $validator->errors();
+                $errors = json_decode($errors);
+
+                return response()->json([
+                    'success' => false,
+                    'message' => $errors
+                ], 422);
+            } else {
+                if ($request->has('repair')) {
+                    $repair = Repair::findOrFail($request->id)->update(['state' => '0']);
+                }
+
+                $approval = Approval::create([
+                    'ids' => Carbon::now()->timestamp,
+                    'remark' => $request->remark,
+                    'diagnostic_id' => $request->diagnostic,
+                    'user_id' =>Auth::user()->id,
+                    'site_id' =>Auth::user()->id,
+                ]);
+            }
+            return response()->json(['id' => $request->id, 'reference' => $approval->diagnostic->state->reference]);
         } else {
-            $pieces = Demand_piece::where('demand_id', $id)->get();
-            return response()->json($pieces);
+            return view('errors.500');
         }
+
+}
+
+public
+function show(Request $request, $id)
+{
+    if ($id == "waiting") {
+        $demands = Demand::where('state', '0')->orderBy('created_at', 'desc')->get();
+        return view('approval.includes.piece', ['demands' => $demands])->with(['id' => $id]);
+    } elseif ($id == "validated") {
+        $demands = Demand::where('state', '1')->orWhere('state', '2')->orWhere('state', '3')->orderBy('created_at', 'desc')->get();
+        return view('approval.includes.piece', ['demands' => $demands])->with(['id' => $id]);
+    } else {
+        $pieces = Demand_piece::where('demand_id', $id)->get();
+        return response()->json($pieces);
+    }
+}
+
+public
+function descriptions(Request $request, $id, $type)
+{
+    //if ($request->ajax()){
+    if ($type == 'repair') {
+        $descriptions = Repair_description::where('repair_id', $id)->orderBy('created_at', 'desc')->get();
+        return response()->json($descriptions);
+    }
+    /*}else{
+        return view('errors.500');
+    }*/
+}
+
+public
+function edit($id)
+{
+    //
+}
+
+public
+function update(Request $request, $id)
+{
+    if ($request->ajax()) {
+        if ($request->has('piece')) {
+            $messages = array(
+                'piece.required' => "Les information de la Pièce à ajouter sont obligatoire",
+                'quantity.required' => "La Quantité est obligatoire",
+            );
+            $rules = array(
+                'piece' => 'bail|required|min:1',
+                'quantity' => 'bail|required|min:1',
+            );
+            $validator = Validator::make($request->all(), $rules, $messages);
+            if ($validator->fails()) {
+                $errors = $validator->errors();
+                $errors = json_decode($errors);
+
+                return response()->json([
+                    'success' => false,
+                    'message' => $errors
+                ], 422);
+            } else {
+                $demand = Demand::findOrFail($id)->update(['state' => '1']);
+                for ($i = 0; $i < count($request->piece); $i++) {
+                    $demand_piece = Demand_piece::create([
+                        'demand_id' => $id,
+                        'piece' => $request->piece[$i],
+                        'quantity' => $request->quantity[$i],
+                    ]);
+                }
+            }
+        } else {
+            $demand = Demand::findOrFail($id)->update(['state' => '1']);
+        }
+        return response()->json(['id' => $id, 'reference' => $request->reference]);
+    } else {
+        return view('errors.500');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+}
 
-    public function update(Request $request, $id)
-    {
-       if ($request->ajax()){
-           if ($request->has('piece')){
-               $messages = array(
-                   'piece.required' => "Les information de la Pièce à ajouter sont obligatoire",
-                   'quantity.required' => "La Quantité est obligatoire",
-               );
-               $rules = array(
-                   'piece' => 'bail|required|min:1',
-                   'quantity' => 'bail|required|min:1',
-               );
-               $validator = Validator::make($request->all(), $rules, $messages);
-               if ($validator->fails()) {
-                   $errors = $validator->errors();
-                   $errors = json_decode($errors);
-
-                   return response()->json([
-                       'success' => false,
-                       'message' => $errors
-                   ], 422);
-               } else {
-                   $demand = Demand::findOrFail($id)->update(['state' => '1']);
-                   for ($i = 0; $i < count($request->piece); $i++) {
-                       $demand_piece = Demand_piece::create([
-                           'demand_id' => $id,
-                           'piece' => $request->piece[$i],
-                           'quantity' => $request->quantity[$i],
-                       ]);
-                   }
-               }
-           }else{
-               $demand = Demand::findOrFail($id)->update(['state' => '1']);
-           }
-           return response()->json(['id' => $id, 'reference' => $request->reference]);
-       }else{
-           return view('errors.500');
-       }
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+/**
+ * Remove the specified resource from storage.
+ *
+ * @param  int $id
+ * @return \Illuminate\Http\Response
+ */
+public
+function destroy($id)
+{
+    //
+}
 }
