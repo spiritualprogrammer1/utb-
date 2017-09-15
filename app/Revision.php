1cafc37bcc86016b5425e7edfe2fa971b6a6294b
Lutte Patrick OKAMANGO <lu2tepok@outlook.com>
2017-09-15 17:45:30 +0000

    Good until now

---
+++ app/Revision.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Revision extends Model
+{
+    protected $fillable = ['ids', 'diagnostic_id', 'site_id', 'state', 'user_id'];
+
+    public function diagnostic()
+    {
+        return $this->belongsTo(Diagnostic::class);
+    }
+
+}
