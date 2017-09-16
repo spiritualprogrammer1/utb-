b279716511ddd7a6c7ba455f40f62f1faeb57097
Lutte Patrick OKAMANGO <lu2tepok@outlook.com>
2017-09-16 19:03:30 +0000

    Good until now

--- resources/views/approval/includes/piece.blade.php
+++ resources/views/approval/includes/piece.blade.php
@@ -1,8 +1,9 @@
 <table class="table table-striped m-b-none capitalize" id="approvalTable">
     <thead>
     <tr>
-        <th width="5"></th>
-        <th width="5"></th>
+        <th width="1"></th>
+        <th width="1"></th>
+        <th>Reference</th>
         <th>Reference OT</th>
         <th>Immatriculation</th>
         <th>Chassis</th>
@@ -15,29 +16,31 @@
     </tr>
     </thead>
     <tbody>
-    @foreach($diagnostics as $key=>$diagnostic)
-        <tr id="approval{{$diagnostic->id}}" class="animated fadeInDown">
+    @foreach($demands as $key=>$demand)
+        <tr id="approval{{$demand->id}}" class="animated fadeInLeft">
             <td>{{$key + 1}}</td>
             <td>
-                <a href="#" id="{{$diagnostic->id}}" class=""><i
+                <a href="#" id="{{$demand->id}}" class=""><i
                             class="fa fa-search-plus text-muted"></i></a>
             </td>
-            <td class="uppercase">{{$diagnostic->process->reference}}</td>
-            <td class="uppercase">{{$diagnostic->process->state->bus->matriculation}}</td>
-            <td class="uppercase">{{$diagnostic->process->state->bus->chassis}}</td>
-            <td>{{$diagnostic->process->state->bus->model->brand->name." ".$diagnostic->process->state->bus->model->name}}</td>
+            <td class="uppercase text-danger-dker">{{$demand->diagnostic->state->reference}}</td>
+            <td class="uppercase text-danger-dker">{{$demand->diagnostic->state->reference}}</td>
+            <td class="uppercase text-primary-dker">{{$demand->diagnostic->state->bus->matriculation}}</td>
+            <td class="uppercase">{{$demand->diagnostic->state->bus->chassis}}</td>
+            <td>{{$demand->diagnostic->state->bus->model->brand->name." ".$demand->diagnostic->state->bus->model->name}}</td>
             @if($id == "validated")
-                <td>{{\Jenssegers\Date\Date::parse($diagnostic->updated_at)->format('j M Y')}}</td>
+                <td>{{\Jenssegers\Date\Date::parse($demand->diagnostic->updated_at)->format('j M Y')}}</td>
             @else
-                <td>{{\Jenssegers\Date\Date::parse($diagnostic->created_at)->format('j M Y')}}</td>
-                <td><a href="#" id="{{$diagnostic->id}}" onclick="waiting(this)"
-                       data-car="{{$diagnostic->process->state->bus->model->brand->name." ".$diagnostic->process->state->bus->model->name}}"
-                       data-matriculation="{{$diagnostic->process->state->bus->matriculation}}"
-                       data-ot="{{$diagnostic->process->reference}}">
+                <td>{{\Jenssegers\Date\Date::parse($demand->created_at)->format('j M Y')}}</td>
+                <td><a href="#" id="{{$demand->id}}" onclick="waiting(this)"
+                       data-car="{{$demand->diagnostic->state->bus->model->brand->name." ".$demand->diagnostic->state->bus->model->name}}"
+                       data-matriculation="{{$demand->diagnostic->state->bus->matriculation}}"
+                       data-ot="{{$demand->diagnostic->state->reference}}">
                         <i class="fa fa-pencil"></i></a></td>
             @endif
         </tr>
     @endforeach
+
     </tbody>
 </table>
 <script>
